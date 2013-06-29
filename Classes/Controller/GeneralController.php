62e4de218621b2980ce41a817b6fb921cc8badc0
Alex Kellner <alexander.kellner@in2code.de>
2013-06-29 15:38:11 +0200

    StoreInDatabase: Working Commit

--- Classes/Controller/GeneralController.php
+++ Classes/Controller/GeneralController.php
@@ -328,7 +328,7 @@ class GeneralController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControll
 
 		// store in database: store values in any wanted table
 		Div::storeInDatabasePreflight($user, $this->config, $this->cObj, $this->objectManager);
-die('xyzzyx');
+
 		// frontend redirect (if activated via TypoScript)
 		$this->redirectByAction($action);
 
