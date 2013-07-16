371520d8aa5e9ce5ae1f861e929ed4bfda5f30a4
Alex Kellner <alexander.kellner@in2code.de>
2013-07-16 20:47:59 +0200

    #50084 Password ServerValidator kicked in even no password field is shown

--- Classes/Domain/Validator/PasswordValidator.php
+++ Classes/Domain/Validator/PasswordValidator.php
@@ -1,7 +1,31 @@
 <?php
 namespace In2\Femanager\Domain\Validator;
 
+use \TYPO3\CMS\Core\Utility\GeneralUtility;
+
 class PasswordValidator extends \TYPO3\CMS\Extbase\Validation\Validator\AbstractValidator {
+
+	/**
+	 * configurationManager
+	 *
+	 * @var \TYPO3\CMS\Extbase\Configuration\ConfigurationManager
+	 * @inject
+	 */
+	public $configurationManager;
+
+	/**
+	 * Content Object
+	 *
+	 * @var object
+	 */
+	public $cObj;
+
+	/**
+	 * Action Name
+	 *
+	 * @var \string
+	 */
+	protected $actionName;
 
 	/**
 	 * Validation of given Params
@@ -10,6 +34,13 @@ class PasswordValidator extends \TYPO3\CMS\Extbase\Validation\Validator\Abstract
 	 * @return bool
 	 */
 	public function isValid($user) {
+		$this->init();
+
+		// if password fields are not shown
+		if (!$this->passwordFieldsAdded()) {
+			return TRUE;
+		}
+
 		$piVars = \TYPO3\CMS\Core\Utility\GeneralUtility::_GP('tx_femanager_pi1');
 		$password = $user->getPassword();
 		$passwordRepeat = $piVars['password_repeat'];
@@ -20,6 +51,34 @@ class PasswordValidator extends \TYPO3\CMS\Extbase\Validation\Validator\Abstract
 		}
 
 		return TRUE;
+	}
+
+	/**
+	 * Check if password fields are added with flexform
+	 *
+	 * @return bool
+	 */
+	protected function passwordFieldsAdded() {
+		$flexFormValues = GeneralUtility::xml2array($this->cObj->data['pi_flexform']);
+		$fields = $flexFormValues['data'][$this->actionName]['lDEF']['settings.' . $this->actionName . '.fields']['vDEF'];
+		if (empty($fields) || GeneralUtility::inList($fields, 'password')) {
+			// password fields are added to form
+			return TRUE;
+		}
+
+		// password fields are not added to form
+		return FALSE;
+	}
+
+	/**
+	 * Initialize Validator Function
+	 *
+	 * @return void
+	 */
+	protected function init() {
+		$this->cObj = $this->configurationManager->getContentObject();
+		$piVars = GeneralUtility::_GP('tx_femanager_pi1');
+		$this->actionName = $piVars['__referrer']['@action'];
 	}
 }
 ?>
