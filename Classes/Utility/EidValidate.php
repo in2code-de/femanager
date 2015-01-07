3f3508622b6fe18f345a580c772d689d16586bf7
Stefan Neufeind <typo3.neufeind@speedpartner.de>
2015-01-07 14:12:08 +0100

    [BUGFIX] eID-validation: Allow id-param via POST/GET
    So far the id-parameter was only used if sent as a
    GET-parameter. Meanwhile AJAX-validation works via
    POST and also sends the id-parameter via POST.
    
    So this is a follow-up to change-id
    I8aedb357e9140b10804aa5b8eda60776481861a7
    
    Change-Id: I4f7cda57827e5c0f3ee1bd2d6a2786cd6ccc4214
    Resolves: #64164
    Releases: master
    Reviewed-on: http://review.typo3.org/35862
    Reviewed-by: Stefan Neufeind <typo3.neufeind@speedpartner.de>
    Tested-by: Stefan Neufeind <typo3.neufeind@speedpartner.de>

--- Classes/Utility/EidValidate.php
+++ Classes/Utility/EidValidate.php
@@ -86,7 +86,7 @@ class EidValidate {
 		$this->bootstrap = new \TYPO3\CMS\Extbase\Core\Bootstrap();
 
 		$userObj = \TYPO3\CMS\Frontend\Utility\EidUtility::initFeUser();
-		$pid = (GeneralUtility::_GET('id') ? GeneralUtility::_GET('id') : 1);
+		$pid = (GeneralUtility::_GP('id') ? GeneralUtility::_GP('id') : 1);
 		$GLOBALS['TSFE'] = GeneralUtility::makeInstance(
 			'TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController',
 			$TYPO3_CONF_VARS,
