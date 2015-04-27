ebd832e4fe2e28cff25837d81af5539acef3b079
Alex Kellner <alexander.kellner@in2code.de>
2015-04-27 16:13:29 +0200

    [TASK] Updated dependencies for fluid and typo3

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,8 +35,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'extbase' => '6.2.0-7.2.99',
-			'fluid' => '6.2.0-6.2.99',
-			'typo3' => '6.2.0-6.2.99',
+			'fluid' => '6.2.0-7.2.99',
+			'typo3' => '6.2.0-7.2.99',
 		),
 		'conflicts' => array(
 		),
