62444f0675e7dde125627f9c41add8500fdd0827
Alex Kellner <alexander.kellner@in2code.de>
2016-02-29 21:10:34 +0100

    [FEATURE] Log changes more detailed
    Add signal to log function
    add additionalproperties call if useful
    
    related: #73634

--- Classes/Controller/AbstractController.php
+++ Classes/Controller/AbstractController.php
@@ -232,8 +232,8 @@ abstract class AbstractController extends ActionController
     {
 
         // send notify email to admin
+        $existingUser = $this->userRepository->findByUid($user->getUid());
         if ($this->settings['edit']['notifyAdmin']) {
-            $existingUser = $this->userRepository->findByUid($user->getUid());
             $this->sendMailService->send(
                 'updateNotify',
                 StringUtility::makeEmailArray(
@@ -254,7 +254,7 @@ abstract class AbstractController extends ActionController
         $this->userRepository->update($user);
         $this->persistenceManager->persistAll();
         $this->signalSlotDispatcher->dispatch(__CLASS__, __FUNCTION__ . 'AfterPersist', [$user, $this]);
-        LogUtility::log(Log::STATUS_PROFILEUPDATED, $user);
+        LogUtility::log(Log::STATUS_PROFILEUPDATED, $user, ['existingUser' => $existingUser]);
         $this->redirectByAction('edit');
         $this->addFlashMessage(LocalizationUtility::translate('update'));
     }
@@ -284,7 +284,7 @@ abstract class AbstractController extends ActionController
             ],
             $this->config['edit.']['email.']['updateRequest.']
         );
-        LogUtility::log(Log::STATUS_PROFILEUPDATEREFUSEDADMIN, $user);
+        LogUtility::log(Log::STATUS_PROFILEUPDATEREFUSEDADMIN, $user, ['dirtyProperties' => $dirtyProperties]);
         $this->redirectByAction('edit', 'requestRedirect');
         $this->addFlashMessage(LocalizationUtility::translate('updateRequest'));
     }
