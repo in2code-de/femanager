5c3be6599b813c2ca07e6c95740904b50aeccc40
Alex Kellner <alexander.kellner@in2code.de>
2017-07-12 10:12:34 +0200

    [TASK] Update dependencies in ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,9 +26,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.6.0',
     'constraints' => [
         'depends' => [
-            'extbase' => '7.6.0-7.99.99',
-            'fluid' => '7.6.0-7.99.99',
-            'typo3' => '7.6.0-7.99.99',
+            'typo3' => '7.6.0-8.99.99',
             'php' => '5.5.0-7.99.99',
         ],
         'conflicts' => [],
