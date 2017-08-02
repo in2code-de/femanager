d6f43a3b1490942259e9c2b271ae5f37e5383213
Alex Kellner <alexander.kellner@in2code.de>
2017-08-02 10:27:07 +0200

    [TASK] Refactor SendMailService class

--- Classes/Domain/Service/SendMailService.php
+++ Classes/Domain/Service/SendMailService.php
@@ -1,67 +1,43 @@
 <?php
 namespace In2code\Femanager\Domain\Service;
 
+use In2code\Femanager\Utility\FrontendUtility;
+use In2code\Femanager\Utility\ObjectUtility;
 use In2code\Femanager\Utility\TemplateUtility;
 use TYPO3\CMS\Core\Mail\MailMessage;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
-
-/***************************************************************
- *  Copyright notice
- *
- *  (c) 2013 Alex Kellner <alexander.kellner@in2code.de>, in2code
- *
- *  All rights reserved
- *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
 
 /**
- * SendMail Function
- *
- * @package femanager
- * @license http://www.gnu.org/licenses/gpl.html
- *          GNU General Public License, version 3 or later
+ * Class SendMailService
  */
 class SendMailService
 {
-
-    /**
-     * objectManager
-     *
-     * @var \TYPO3\CMS\Extbase\Object\ObjectManager
-     * @inject
-     */
-    protected $objectManager;
-
-    /**
-     * configurationManager
-     *
-     * @var \TYPO3\CMS\Extbase\Configuration\ConfigurationManager
-     * @inject
-     */
-    protected $configurationManager;
 
     /**
      * Content Object
      *
      * @var object
      */
-    public $cObj;
+    public $contentObject;
+
+    /**
+     * SendMailService constructor.
+     */
+    public function __construct()
+    {
+        $this->contentObject = ObjectUtility::getContentObject();
+    }
+
+    /**
+     * @param array $variables
+     * @return void
+     */
+    protected function contentObjectStart(array $variables)
+    {
+        if (!empty($variables['user']) && method_exists($variables['user'], '_getProperties')) {
+            $this->contentObject->start($variables['user']->_getProperties());
+        }
+    }
 
     /**
      * Generate and send Email
@@ -74,107 +50,29 @@ class SendMailService
      * @param array $typoScript Add TypoScript to overwrite values
      * @return bool mail was sent?
      */
-    public function send($template, $receiver, $sender, $subject, $variables = [], $typoScript = [])
-    {
-        // config
-        $email = $this->objectManager->get(MailMessage::class);
-        $this->cObj = $this->configurationManager->getContentObject();
-        if($this->cObj == null) {
-            $this->cObj = $this->objectManager->get(ContentObjectRenderer::class);
+    public function send(
+        string $template,
+        array $receiver,
+        array $sender,
+        string $subject,
+        array $variables = [],
+        array $typoScript = []
+    ): bool {
+        if ($this->isMailEnabled($typoScript, $receiver)) {
+            $this->contentObjectStart($variables);
+            $email = ObjectUtility::getObjectManager()->get(MailMessage::class);
+            $variables = $this->embedImages($variables, $typoScript, $email);
+            $this->prepareMailObject($template, $receiver, $sender, $subject, $variables, $email);
+            $this->overwriteEmailReceiver($typoScript, $email);
+            $this->overwriteEmailSender($typoScript, $email);
+            $this->setSubject($typoScript, $email);
+            $this->setCc($typoScript, $email);
+            $this->setPriority($typoScript, $email);
+            $this->setAttachments($typoScript, $email);
+            $email->send();
+            return $email->isSent();
         }
-        if (!empty($variables['user']) && method_exists($variables['user'], '_getProperties')) {
-            $this->cObj->start($variables['user']->_getProperties());
-        }
-        if (!$this->cObj->cObjGetSingle($typoScript['_enable'], $typoScript['_enable.']) || count($receiver) === 0) {
-            return false;
-        }
-
-        // add embed images to mail body
-        if ($this->cObj->cObjGetSingle($typoScript['embedImage'], $typoScript['embedImage.'])) {
-            $images = GeneralUtility::trimExplode(
-                ',',
-                $this->cObj->cObjGetSingle($typoScript['embedImage'], $typoScript['embedImage.']),
-                true
-            );
-            $imageVariables = [];
-            foreach ($images as $image) {
-                $imageVariables[] = $email->embed(\Swift_Image::fromPath($image));
-            }
-            $variables = array_merge($variables, ['embedImages' => $imageVariables]);
-        }
-
-        /**
-         * Generate and send Email
-         */
-        $email
-            ->setTo($receiver)
-            ->setFrom($sender)
-            ->setSubject($subject)
-            ->setCharset($GLOBALS['TSFE']->metaCharset)
-            ->setBody($this->getMailBody($template, $variables), 'text/html');
-
-        // overwrite email receiver
-        if (
-            $this->cObj->cObjGetSingle(
-                $typoScript['receiver.']['email'],
-                $typoScript['receiver.']['email.']
-            )
-            &&
-            $this->cObj->cObjGetSingle(
-                $typoScript['receiver.']['name'],
-                $typoScript['receiver.']['name.']
-            )
-        ) {
-            $email->setTo(
-                [
-                    $this->cObj->cObjGetSingle($typoScript['receiver.']['email'], $typoScript['receiver.']['email.']) =>
-                        $this->cObj->cObjGetSingle($typoScript['receiver.']['name'], $typoScript['receiver.']['name.'])
-                ]
-            );
-        }
-
-        // overwrite email sender
-        if ($this->cObj->cObjGetSingle($typoScript['sender.']['email'], $typoScript['sender.']['email.']) &&
-            $this->cObj->cObjGetSingle($typoScript['sender.']['name'], $typoScript['sender.']['name.'])
-        ) {
-            $email->setFrom(
-                [
-                    $this->cObj->cObjGetSingle($typoScript['sender.']['email'], $typoScript['sender.']['email.']) =>
-                        $this->cObj->cObjGetSingle($typoScript['sender.']['name'], $typoScript['sender.']['name.'])
-                ]
-            );
-        }
-
-        // overwrite email subject
-        if ($this->cObj->cObjGetSingle($typoScript['subject'], $typoScript['subject.'])) {
-            $email->setSubject($this->cObj->cObjGetSingle($typoScript['subject'], $typoScript['subject.']));
-        }
-
-        // overwrite email CC receivers
-        if ($this->cObj->cObjGetSingle($typoScript['cc'], $typoScript['cc.'])) {
-            $email->setCc($this->cObj->cObjGetSingle($typoScript['cc'], $typoScript['cc.']));
-        }
-
-        // overwrite email priority
-        if ($this->cObj->cObjGetSingle($typoScript['priority'], $typoScript['priority.'])) {
-            $email->setPriority($this->cObj->cObjGetSingle($typoScript['priority'], $typoScript['priority.']));
-        }
-
-        // add attachments from typoscript
-        if ($this->cObj->cObjGetSingle($typoScript['attachments'], $typoScript['attachments.'])) {
-            $files = GeneralUtility::trimExplode(
-                ',',
-                $this->cObj->cObjGetSingle($typoScript['attachments'], $typoScript['attachments.']),
-                true
-            );
-            foreach ($files as $file) {
-                $email->attach(\Swift_Attachment::fromPath($file));
-            }
-        }
-
-        $email->send();
-
-        return $email->isSent();
+        return false;
     }
 
     /**
@@ -193,6 +91,153 @@ class SendMailService
     }
 
     /**
+     * @param array $variables
+     * @param array $typoScript
+     * @param MailMessage $email
+     * @return array
+     */
+    protected function embedImages(array $variables, array $typoScript, MailMessage $email): array
+    {
+        if ($this->contentObject->cObjGetSingle($typoScript['embedImage'], $typoScript['embedImage.'])) {
+            $images = GeneralUtility::trimExplode(
+                ',',
+                $this->contentObject->cObjGetSingle($typoScript['embedImage'], $typoScript['embedImage.']),
+                true
+            );
+            $imageVariables = [];
+            foreach ($images as $image) {
+                $imageVariables[] = $email->embed(\Swift_Image::fromPath($image));
+            }
+            $variables = array_merge($variables, ['embedImages' => $imageVariables]);
+        }
+        return $variables;
+    }
+
+    /**
+     * @param string $template
+     * @param array $receiver
+     * @param array $sender
+     * @param string $subject
+     * @param array $variables
+     * @param MailMessage $email
+     * @return void
+     */
+    protected function prepareMailObject(
+        string $template,
+        array $receiver,
+        array $sender,
+        string $subject,
+        array $variables,
+        MailMessage $email
+    ) {
+        $email
+            ->setTo($receiver)
+            ->setFrom($sender)
+            ->setSubject($subject)
+            ->setCharset(FrontendUtility::getCharset())
+            ->setBody($this->getMailBody($template, $variables), 'text/html');
+    }
+
+    /**
+     * @param array $typoScript
+     * @param MailMessage $email
+     * @return void
+     */
+    protected function overwriteEmailReceiver(array $typoScript, MailMessage $email)
+    {
+        if ($this->contentObject->cObjGetSingle($typoScript['receiver.']['email'], $typoScript['receiver.']['email.'])
+            && $this->contentObject->cObjGetSingle($typoScript['receiver.']['name'], $typoScript['receiver.']['name.'])
+        ) {
+            $emailAddress = $this->contentObject->cObjGetSingle(
+                $typoScript['receiver.']['email'],
+                $typoScript['receiver.']['email.']
+            );
+            $name = $this->contentObject->cObjGetSingle(
+                $typoScript['receiver.']['name'],
+                $typoScript['receiver.']['name.']
+            );
+            $email->setTo([$emailAddress => $name]);
+        }
+    }
+
+    /**
+     * @param array $typoScript
+     * @param MailMessage $email
+     * @return void
+     */
+    protected function overwriteEmailSender(array $typoScript, MailMessage $email)
+    {
+        if ($this->contentObject->cObjGetSingle($typoScript['sender.']['email'], $typoScript['sender.']['email.']) &&
+            $this->contentObject->cObjGetSingle($typoScript['sender.']['name'], $typoScript['sender.']['name.'])
+        ) {
+            $emailAddress = $this->contentObject->cObjGetSingle(
+                $typoScript['sender.']['email'],
+                $typoScript['sender.']['email.']
+            );
+            $name = $this->contentObject->cObjGetSingle(
+                $typoScript['sender.']['name'],
+                $typoScript['sender.']['name.']
+            );
+            $email->setFrom([$emailAddress => $name]);
+        }
+    }
+
+    /**
+     * @param array $typoScript
+     * @param MailMessage $email
+     * @return void
+     */
+    protected function setSubject(array $typoScript, MailMessage $email)
+    {
+        if ($this->contentObject->cObjGetSingle($typoScript['subject'], $typoScript['subject.'])) {
+            $email->setSubject($this->contentObject->cObjGetSingle($typoScript['subject'], $typoScript['subject.']));
+        }
+    }
+
+    /**
+     * @param array $typoScript
+     * @param MailMessage $email
+     * @return void
+     */
+    protected function setCc(array $typoScript, MailMessage $email)
+    {
+        if ($this->contentObject->cObjGetSingle($typoScript['cc'], $typoScript['cc.'])) {
+            $email->setCc($this->contentObject->cObjGetSingle($typoScript['cc'], $typoScript['cc.']));
+        }
+    }
+
+    /**
+     * @param array $typoScript
+     * @param MailMessage $email
+     * @return void
+     */
+    protected function setPriority(array $typoScript, MailMessage $email)
+    {
+        if ($this->contentObject->cObjGetSingle($typoScript['priority'], $typoScript['priority.'])) {
+            $email->setPriority($this->contentObject->cObjGetSingle($typoScript['priority'], $typoScript['priority.']));
+        }
+    }
+
+    /**
+     * @param array $typoScript
+     * @param MailMessage $email
+     * @return void
+     */
+    protected function setAttachments(array $typoScript, MailMessage $email)
+    {
+        if ($this->contentObject->cObjGetSingle($typoScript['attachments'], $typoScript['attachments.'])) {
+            $files = GeneralUtility::trimExplode(
+                ',',
+                $this->contentObject->cObjGetSingle($typoScript['attachments'], $typoScript['attachments.']),
+                true
+            );
+            foreach ($files as $file) {
+                $email->attach(\Swift_Attachment::fromPath($file));
+            }
+        }
+    }
+
+    /**
      * Get path and filename for mail template
      *
      * @param string $fileName
@@ -201,5 +246,16 @@ class SendMailService
     protected function getRelativeEmailPathAndFilename($fileName)
     {
         return TemplateUtility::getTemplatePath('Email/' . ucfirst($fileName) . '.html');
+    }
+
+    /**
+     * @param array $typoScript
+     * @param array $receiver
+     * @return bool
+     */
+    protected function isMailEnabled(array $typoScript, array $receiver): bool
+    {
+        return $this->contentObject->cObjGetSingle($typoScript['_enable'], $typoScript['_enable.'])
+            && count($receiver) > 0;
     }
 }
