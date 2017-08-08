07d137f72ebce4022a56c54642c722d0de1145e3
Alex Kellner <alexander.kellner@in2code.de>
2017-08-08 13:23:47 +0200

    [BUGFIX] Change $TCA to $GLOBALS['TCA'] an anonymous function in ext_tables.php

--- ext_tables.php
+++ ext_tables.php
@@ -49,7 +49,7 @@ call_user_func(function () {
      * Flexform
      */
     $pluginSignature = str_replace('_', '', 'femanager') . '_pi1';
-    $TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
         $pluginSignature,
         'FILE:EXT:femanager/Configuration/FlexForms/FlexFormPi1.xml'
@@ -58,5 +58,5 @@ call_user_func(function () {
     /**
      * Disable non needed fields in tt_content
      */
-    $TCA['tt_content']['types']['list']['subtypes_excludelist']['femanager_pi1'] = 'select_key';
+    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['femanager_pi1'] = 'select_key';
 });
