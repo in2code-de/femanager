222c4ae3aeb3a9e26bbfd0466cff30beb5fa5571
Alex Kellner <alexander.kellner@in2code.de>
2017-08-10 16:56:18 +0200

    [TASK] Add autoload information for TYPO3 instances in classic mode

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,4 +35,9 @@ $EM_CONF[$_EXTKEY] = [
             'static_info_tables' => '6.0.0-6.99.99'
         ],
     ],
+    'autoload' => [
+        'psr-4' => [
+            'In2code\\Femanager\\' => 'Classes'
+        ]
+    ],
 ];
