d506eed23fe5c85773bc618e314a6ce53fe08472
Dima <dmytro.nozdrin.stratis@gmail.com>
2018-11-15 19:07:46 +0200

    [TASK] Migrate femanager:Misc.BackendNewLink

--- Classes/ViewHelpers/Misc/BackendNewLinkViewHelper.php
+++ Classes/ViewHelpers/Misc/BackendNewLinkViewHelper.php
@@ -3,23 +3,39 @@ declare(strict_types=1);
 namespace In2code\Femanager\ViewHelpers\Misc;
 
 use In2code\Femanager\Utility\BackendUtility;
-use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
 
 /**
  * Class BackendNewLinkViewHelper
  */
 class BackendNewLinkViewHelper extends AbstractViewHelper
 {
+    use CompileWithRenderStatic;
+
+    /**
+     * initialize arguments
+     */
+    public function initializeArguments()
+    {
+        $this->registerArgument('tableName', 'string', 'Records table name (like "fe_users")', true);
+        $this->registerArgument('addReturnUrl', 'bool', 'Add current URI as returnUrl', false, true);
+    }
 
     /**
      * Get an URI for new records in backend
      *
-     * @param string $tableName
-     * @param bool $addReturnUrl
+     * @param array $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
      * @return string
      */
-    public function render(string $tableName, bool $addReturnUrl = true): string
-    {
-        return BackendUtility::getBackendNewUri($tableName, BackendUtility::getPageIdentifier(), $addReturnUrl);
+    public static function renderStatic(
+        array $arguments,
+        \Closure $renderChildrenClosure,
+        RenderingContextInterface $renderingContext
+    ):string {
+        return BackendUtility::getBackendNewUri($arguments['tableName'], BackendUtility::getPageIdentifier(), $arguments['addReturnUrl']);
     }
 }
