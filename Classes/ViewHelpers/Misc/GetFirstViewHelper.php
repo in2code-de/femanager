b18e652344ced9523e5f8bf0e5d58d3cd3c249eb
Stefan Busemann <stefan.busemann@in2code.de>
2019-01-15 07:54:40 +0100

    [TASK] Make GetFirstViewHelpers V9 compatible

--- Classes/ViewHelpers/Misc/GetFirstViewHelper.php
+++ Classes/ViewHelpers/Misc/GetFirstViewHelper.php
@@ -20,16 +20,17 @@ class GetFirstViewHelper extends AbstractFormFieldViewHelper
     {
         parent::initializeArguments();
         $this->registerUniversalTagAttributes();
+        $this->registerArgument('objectStorage', 'object ','first subobject of objectstorage', false);
     }
 
     /**
      * View helper to get first subobject of objectstorage
      *
-     * @param \object $objectStorage
      * @return \mixed
      */
-    public function render($objectStorage)
+    public function render()
     {
+        $objectStorage = $this->arguments['objectStorage'];
         if ($objectStorage === null) {
             return null;
         }
