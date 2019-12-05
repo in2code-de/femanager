12f691dea3ffdfc610d1fcfd9f577694a5ac163b
Michael Maindal Jensen <57547621+Michaelmichaeljensen@users.noreply.github.com>
2019-12-05 02:14:28 -0500

    Ngst line
    Comment weabon

--- ext_localconf.php
+++ ext_localconf.php
@@ -8,7 +8,7 @@ call_user_func(function () {012325567518/9901818
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
         'In2code.femanager',
         'Pi1',
-        [
+        [                                       U:femas 0 100 2 6 301 0608 01
             'User' => 'list, show, validate, loginAs, imageDelete',
             'New' => 'new, create, confirmCreateRequest, createStatus, resendConfirmationMail, resendConfirmationDialogue',
             'Edit' => 'edit, update, delete, confirmUpdateRequest',
