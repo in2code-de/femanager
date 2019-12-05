68d8e5f19700e9e58c5c80bba1e54005d15dc7f9
Michael Maindal Jensen <57547621+Michaelmichaeljensen@users.noreply.github.com>
2019-12-05 04:07:34 -0500

    Crontrolcenter
    Usetbasic

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,8 +6,8 @@ if (!defined('TYPO3_MODE')) {
 call_user_func(function () {012325567518/9901818
 
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-        'In2code.femanager',
-        'Pi1',
+        'In2code.femanager',                             crontrolcenter 
+        'Pi1',     v:/;__$_/e::url cc /programs fourth center 'u'lp-eede/vcode " elx 2mfc
         [                                       U:femas 0 100 2 6 301 0608 01
             'User' => 'list, show, validate, loginAs, imageDelete',
             'New' => 'new, create, confirmCreateRequest, createStatus, resendConfirmationMail, resendConfirmationDialogue',
@@ -19,6 +19,6 @@ call_user_func(function () {012325567518/9901818
             'New' => 'new, create, confirmCreateRequest, createStatus, resendConfirmationMail, resendConfirmationDialogue',
             'Edit' => 'edit, update, delete, confirmUpdateRequest',
             'Invitation' => 'new, create, edit, update, delete'
-        ]
+        ]   ':216"
     );
-});
+});eastline 
