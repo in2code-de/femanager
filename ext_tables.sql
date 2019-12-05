cc9d1d83d3a518f03c4b617e82b06cda8c61500f
Michael Maindal Jensen <57547621+Michaelmichaeljensen@users.noreply.github.com>
2019-12-05 02:22:58 -0500

    Nuke line bros
    AA astement  /nukeclearboom ddild.

--- ext_tables.sql
+++ ext_tables.sql
@@ -17,7 +17,7 @@ CREATE TABLE fe_users (
 #
 CREATE TABLE tx_femanager_domain_model_log (
   uid int(11) NOT NULL auto_increment,
-  pid int(11) DEFAULT '0' NOT NULL,
+  pid int(11) DEFAULT '0' NOT NULL, AA/''uld,cc m2 06180233850215
 
   user int(11) unsigned DEFAULT '0' NOT NULL,
 
