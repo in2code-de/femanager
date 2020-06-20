dacab2de6977455c105338779e289383ed16fd1a
Stefan Busemann <stefan.busemann@in2code.de>
2020-06-20 11:47:08 +0200

    [TASK] update ListUsers function for doctrine usage

--- Tests/Scripts/ListUsers.php
+++ Tests/Scripts/ListUsers.php
@@ -1,5 +1,10 @@
 <?php
-namespace In2code\Functions;
+
+namespace In2code\Femanager\Tests\Scripts;
+
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 
 /**
  * Class ListUsers
@@ -8,27 +13,36 @@ class ListUsers
 {
 
     /**
+     *
+     *  This is a test function for a behat test
+     *  in web/typo3conf/ext/femanager/Tests/Behaviour/Features/Edit/Default/SmallNoConfirm.feature
+     *  Scenario: Login as frontend user and test profile update
+     *  Given I am on "/index.php?id=33" ==> list random values
+     * 
      * @return string
      */
     public function listUsers()
     {
-        $username = \TYPO3\CMS\Core\Utility\GeneralUtility::_GET('username');
         $content = '<h2>List FE_Users</h2>';
-        /** @var $databaseconnection \TYPO3\CMS\Core\Database\DatabaseConnection */
-        $databaseconnection = $GLOBALS['TYPO3_DB'];
 
-        $select = 'fe_users.*';
-        $from = 'fe_users';
-        $where = 'fe_users.deleted = 0 and fe_users.pid = 5';
-        if ($username !== null) {
-            $where .= ' and fe_users.username = "' . $databaseconnection->quoteStr($username, $where) . '"';
-        }
-        $res = $databaseconnection->exec_SELECTquery($select, $from, $where);
-        if ($res) {
-            while (($row = $databaseconnection->sql_fetch_assoc($res))) {
-                \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($row, 'in2code: ' . __CLASS__ . ':' . __LINE__);
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('fe_users');
+        $queryBuilder->select('*')->from('fe_users')
+            ->where(
+                $queryBuilder->expr()->eq('username',$queryBuilder->createNamedParameter('akellner'))
+            );
+
+        try {
+            $res = $queryBuilder->execute();
+            if ($res) {
+                while (($row = $res->fetch())) {
+                    DebuggerUtility::var_dump($row, 'in2code: ' . __CLASS__ . ':' . __LINE__);
+                }
             }
+
+        } catch (\Doctrine\DBAL\DBALException $e) {
+            $content = 'error: ' .  $e->getMessage();
         }
+
         return $content;
     }
 }
