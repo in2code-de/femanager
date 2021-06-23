9b5eb6b2efdf804fff322de61a3a5f9cb744bf34
Stefan Busemann <stefan.busemann@in2code.de>
2021-06-23 22:22:41 +0200

    [TASK] Add option to run single behat test

---
+++ Tests/Behaviour/behatsf.docker.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo 'start behat for femanager and stop on the first failure'
+../../.Build/vendor/behat/behat/bin/behat --stop-on-failure  --config /app/Tests/Behaviour/behat.docker.yml --tags $1
