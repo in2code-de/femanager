6e2529f3a04dee7fbb4c360e2293572ad1e48f4a
Stefan Busemann <stefan.busemann@in2code.de>
2021-10-13 23:17:50 +0200

    [TASK] Update dev environment for TYPO3
    Missing  / not compatible packages:
    "nimut/testing-framework": "^5.0",
    "behat/mink-goutte-driver": "^1.2",
    "sjbr/sr-freecap": "^2.6",
    "sjbr/static-info-tables": "^6.9",
    "behat/mink-extension": "^2.3",

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,11 +23,11 @@ $EM_CONF[$_EXTKEY] = [
     'modify_tables' => '',
     'clearCacheOnLoad' => 0,
     'lockType' => '',
-    'version' => '6.3.2',
+    'version' => '7.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.0.0-10.4.99',
-            'php' => '7.2.0-7.4.99',
+            'typo3' => '11.0.0-11.5.99',
+            'php' => '7.4.0-7.4.99',
         ],
         'conflicts' => [],
         'suggests' => [
