c4dafb2a9532ea828e2f65f8c1239b42b2125315
Daniel Hoffmann <daniel.hoffmann@in2code.de>
2022-08-18 11:36:22 +0200

    [FIX] Fixes CleanUserGroup DataProcessor
    If there is no usergroup selected in the registration and the form is submitted it brakes before the validation is executed with the error "Cannot unset string offsets" in Line 18 of femanager/Classes/DataProcessor/CleanUserGroup.php

--- Classes/DataProcessor/CleanUserGroup.php
+++ Classes/DataProcessor/CleanUserGroup.php
@@ -15,7 +15,7 @@ class CleanUserGroup extends AbstractDataProcessor
      */
     public function process(array $arguments): array
     {
-        if (empty($arguments['user']['usergroup'][0]) && empty($arguments['user']['usergroup'][0]['__identity'])) {
+        if (is_array($arguments['user']['usergroup']) && empty($arguments['user']['usergroup'][0]) && empty($arguments['user']['usergroup'][0]['__identity'])) {
             unset($arguments['user']['usergroup'][0]);
         }
         return $arguments;
