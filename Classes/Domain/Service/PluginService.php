1738a439fc356996db05a08845494db7f4d1a467
Christine Zoglmeier <christine.zoglmeier@in2code.de>
2023-04-20 10:45:24 +0200

    [TASK] Add PluginService to get names of plugins from request

---
+++ Classes/Domain/Service/PluginService.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+namespace In2code\Femanager\Domain\Service;
+
+class PluginService
+{
+    const ALLOWED_PLUGINS = [
+        'tx_femanager_registration',
+        'tx_femanager_edit',
+        'tx_femanager_list',
+        'tx_femanager_detail',
+        'tx_femanager_invitation',
+        'tx_femanager_resendConfirmationMail'
+    ];
+
+    /**
+     * @return string The name of the femanager plugin in this request
+     */
+    public function getFemanagerPluginNameFromRequest(): string
+    {
+        $request =  $GLOBALS['TYPO3_REQUEST'];
+
+        // only the name of the first femanager plugin is returned
+        foreach ($request->getParsedBody() as $key => $value) {
+            if (in_array($key, self::ALLOWED_PLUGINS)) {
+                return $key;
+            }
+        }
+        return '';
+    }
+
+    public function getAllowedPlugins(): array
+    {
+        return self::ALLOWED_PLUGINS;
+    }
+}
