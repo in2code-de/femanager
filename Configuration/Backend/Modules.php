a80aa6af8277f2ac2a69daaa59e64a0e83458fe6
Christine Zoglmeier <christine.zoglmeier@in2code.de>
2023-05-03 16:17:31 +0200

    [TASK] Restore extension setting for hiding backend module

--- Configuration/Backend/Modules.php
+++ Configuration/Backend/Modules.php
@@ -1,26 +1,30 @@
 <?php
 
 use In2code\Femanager\Controller\UserBackendController;
-
-return [
-    'tx_femanager' => [
-        'parent' => 'web',
-        'position' => ['after' => 'web_info'],
-        'access' => 'user,group',
-        'icon' => 'EXT:femanager/Resources/Public/Icons/Extension.svg',
-        'path' => '/module/web/femanager',
-        'labels' => 'LLL:EXT:femanager/Resources/Private/Language/locallang_mod.xlf',
-        'extensionName' => 'Femanager',
-        'controllerActions' => [
-            UserBackendController::class => [
-                'list',
-                'confirmation',
-                'userLogout',
-                'confirmUser',
-                'refuseUser',
-                'listOpenUserConfirmations',
-                'resendUserConfirmationRequest'
+if (!\In2code\Femanager\Utility\ConfigurationUtility::isDisableModuleActive()) {
+    return [
+        'tx_femanager' => [
+            'parent' => 'web',
+            'position' => ['after' => 'web_info'],
+            'access' => 'user,group',
+            'icon' => 'EXT:femanager/Resources/Public/Icons/Extension.svg',
+            'path' => '/module/web/femanager',
+            'labels' => 'LLL:EXT:femanager/Resources/Private/Language/locallang_mod.xlf',
+            'extensionName' => 'Femanager',
+            'controllerActions' => [
+                UserBackendController::class => [
+                    'list',
+                    'confirmation',
+                    'userLogout',
+                    'confirmUser',
+                    'refuseUser',
+                    'listOpenUserConfirmations',
+                    'resendUserConfirmationRequest'
+                ],
             ],
         ],
-    ],
-];
+    ];
+} else {
+    return [];
+}
+
