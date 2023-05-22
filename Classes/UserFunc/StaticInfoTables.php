976b06907049d934177cf1cada95b31073a7d828
Daniel Hoffmann <daniel.hoffmann@in2code.de>
2023-05-22 14:29:06 +0200

    [TASK] Removes Legacy Code and integrates Country API from core

--- Classes/UserFunc/StaticInfoTables.php
+++ Classes/UserFunc/StaticInfoTables.php
@@ -6,13 +6,21 @@ namespace In2code\Femanager\UserFunc;
 
 use In2code\Femanager\DataProvider\CountryDataProvider;
 use In2code\Femanager\DataProvider\CountryZonesDataProvider;
-use In2code\Femanager\DataProvider\FallbackCountryDataProvider;
-use TYPO3\CMS\Backend\Form\FormDataProvider\TcaSelectItems;
+use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Core\Country\CountryProvider;
+use TYPO3\CMS\Core\Localization\LanguageServiceFactory;
+use TYPO3\CMS\Core\Site\Entity\SiteLanguage;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class StaticInfoTables
 {
+    public function __construct(
+        private readonly CountryProvider $countryProvider,
+        private readonly LanguageServiceFactory $languageServiceFactory
+    ) {
+    }
+
     public function isStaticInfoTablesInstalled(): bool
     {
         return ExtensionManagementUtility::isLoaded('static_info_tables');
@@ -67,11 +75,22 @@ class StaticInfoTables
                 $data['items'][] = [$country->getShortNameEn(), $country->getIsoCodeA3()];
             }
         } else {
-            $fallbackDataProvider = GeneralUtility::makeInstance(FallbackCountryDataProvider::class);
-            $countries = $fallbackDataProvider->getCountries();
-            foreach ($countries as $key => $name) {
-                $data['items'][] = [$name, $key];
+            /**
+             * @var ServerRequestInterface $request
+             */
+            $request = $GLOBALS['TYPO3_REQUEST'];
+            $languageService = null;
+            if ($request->getAttribute('language') instanceof SiteLanguage) {
+                $languageService = $this->languageServiceFactory->createFromSiteLanguage($request->getAttribute('language'));
             }
+
+            $returnArray = [];
+            $countries = $this->countryProvider->getAll();
+            foreach ($countries as $country) {
+                $returnArray[$country->getAlpha3IsoCode()] = $languageService !== null ? $languageService->sL($country->getLocalizedNameLabel()) : $country->getName();
+            }
+            asort($returnArray);
+            $data['items'] = $returnArray;
         }
     }
 }
