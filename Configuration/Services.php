24b477575aa8ed678d5fe8f5cf65c310a82ecf36
Daniel Hoffmann <daniel.hoffmann@in2code.de>
2023-09-20 14:40:29 +0200

    [TASK] Uses check for existing class to determine if static_info_tables is installed

--- Configuration/Services.php
+++ Configuration/Services.php
@@ -4,15 +4,15 @@ namespace In2code\Femanager\DependencyInjection;
 
 use In2code\Femanager\DependencyInjection\CompilerPass\ChangeClassDatamapPass;
 use In2code\Femanager\UserFunc\StaticInfoTables;
+use SJBR\StaticInfoTables\Domain\Model\Country;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use TYPO3\CMS\Core\DependencyInjection\PublicServicePass;
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 return function (ContainerConfigurator $configurator, ContainerBuilder $containerBuilder) {
     $containerBuilder->addCompilerPass(new ChangeClassDatamapPass());
 
-    if (ExtensionManagementUtility::isLoaded('static_info_tables')) {
+    if (class_exists(Country::class)) {
         $containerBuilder->registerForAutoconfiguration(StaticInfoTables::class)->addTag('femanager.userfunc.staticinfotables');
         $containerBuilder->addCompilerPass(new PublicServicePass('femanager.userfunc.staticinfotables'));
     }
