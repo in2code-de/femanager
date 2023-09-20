c04deeca3c7a5ecd3bb40ee3ff7c14c2367e2e26
Daniel Hoffmann <daniel.hoffmann@in2code.de>
2023-09-20 14:10:25 +0200

    [TASK] Adds DI Definition when static_info_tables is present

--- Configuration/Services.php
+++ Configuration/Services.php
@@ -3,9 +3,17 @@
 namespace In2code\Femanager\DependencyInjection;
 
 use In2code\Femanager\DependencyInjection\CompilerPass\ChangeClassDatamapPass;
+use In2code\Femanager\UserFunc\StaticInfoTables;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use TYPO3\CMS\Core\DependencyInjection\PublicServicePass;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 return function (ContainerConfigurator $configurator, ContainerBuilder $containerBuilder) {
     $containerBuilder->addCompilerPass(new ChangeClassDatamapPass());
+
+    if (ExtensionManagementUtility::isLoaded('static_info_tables')) {
+        $containerBuilder->registerForAutoconfiguration(StaticInfoTables::class)->addTag('femanager.userfunc.staticinfotables');
+        $containerBuilder->addCompilerPass(new PublicServicePass('femanager.userfunc.staticinfotables'));
+    }
 };
