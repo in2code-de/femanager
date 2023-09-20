60fb4ec2f96312cefdc0d2ea5491d25cbab60d92
Daniel Hoffmann <daniel.hoffmann@in2code.de>
2023-09-20 12:52:04 +0200

    [TASK] Adds Tests for Invitations

--- Tests/Scripts/ListUsers.php
+++ Tests/Scripts/ListUsers.php
@@ -4,6 +4,8 @@ namespace In2code\Femanager\Tests\Scripts;
 
 use Doctrine\DBAL\DBALException;
 use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Database\Query\Restriction\HiddenRestriction;
+use TYPO3\CMS\Core\Database\Query\Restriction\QueryRestrictionContainerInterface;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 
@@ -68,6 +70,37 @@ class ListUsers
         $content = '<h2>List latest FE_Users</h2>';
         $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('fe_users');
         $queryBuilder->select('*')->from('fe_users')->orderBy('uid', 'desc')->setMaxResults(1);
+
+        try {
+            $res = $queryBuilder->executeQuery();
+            if ($res) {
+                $row = $res->fetchAssociative();
+                $content .= DebuggerUtility::var_dump(
+                    $row,
+                    'in2code: ' . self::class . ':' . __LINE__,
+                    8,
+                    false,
+                    true,
+                    true
+                );
+            }
+        } catch (DBALException $e) {
+            $content = 'error: ' . $e->getMessage();
+        }
+
+        return $content;
+    }
+
+    public function listLatestUserIncludingHidden()
+    {
+        $content = '<h2>List latest hidden FE_User</h2>';
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('fe_users');
+        $queryBuilder->getRestrictions()->removeByType(HiddenRestriction::class);
+        $queryBuilder
+            ->select('*')
+            ->from('fe_users')
+            ->orderBy('uid', 'desc')
+            ->setMaxResults(1);
 
         try {
             $res = $queryBuilder->executeQuery();
