0abcb10b7e7be24b2f7ae65586b09146b16a0edf
Andreas Nedbal <andreas.nedbal@in2code.de>
2025-10-22 13:48:57 +0200

    [TASK] Set default email for admin confirmations to non-user email
    Fixes #108

--- Classes/Controller/NewController.php
+++ Classes/Controller/NewController.php
@@ -480,7 +480,7 @@ class NewController extends AbstractFrontendController
                     $this->settings['new']['confirmByAdmin'] ?? '',
                     $this->settings['new']['email']['createAdminConfirmation']['receiver']['name']['value'] ?? ''
                 ),
-                StringUtility::makeEmailArray($user->getEmail(), $user->getUsername()),
+                ['sender@femanager.org' => 'Sender Name'],
                 'New Registration request',
                 [
                     'user' => $user,
