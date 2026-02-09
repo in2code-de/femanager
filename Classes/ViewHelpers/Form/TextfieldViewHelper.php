980fb0c4ca3eb593275c33ffd74e69358391e3ab
Sebastian Stein <sebastian.stein@in2code.de>
2026-02-09 14:11:42 +0100

    [TASK] backport core chances of TextfieldViewHelper
    Relates: https://projekte.in2code.de/issues/77936

--- Classes/ViewHelpers/Form/TextfieldViewHelper.php
+++ Classes/ViewHelpers/Form/TextfieldViewHelper.php
@@ -10,13 +10,8 @@ use TYPO3\CMS\Fluid\Core\Rendering\RenderingContext;
 use TYPO3\CMS\Fluid\ViewHelpers\Form\AbstractFormFieldViewHelper;
 use TYPO3Fluid\Fluid\Core\ViewHelper\Exception as FluidViewHelperException;
 
-/**
- * Class TextfieldViewHelper
- */
 class TextfieldViewHelper extends AbstractFormFieldViewHelper
 {
-    public $request;
-
     /**
      * @var string
      */
@@ -30,74 +25,15 @@ class TextfieldViewHelper extends AbstractFormFieldViewHelper
     public function initializeArguments(): void
     {
         parent::initializeArguments();
-        $this->registerTagAttribute(
-            'autofocus',
-            'string',
-            'Specifies that an input should automatically get focus when the page loads'
-        );
-        $this->registerTagAttribute(
-            'disabled',
-            'string',
-            'Specifies that the input element should be disabled when the page loads'
-        );
-        $this->registerTagAttribute(
-            'maxlength',
-            'int',
-            'The maxlength attribute of the input field (will not be validated)'
-        );
-        $this->registerTagAttribute(
-            'readonly',
-            'string',
-            'The readonly attribute of the input field'
-        );
-        $this->registerTagAttribute(
-            'size',
-            'int',
-            'The size of the input field'
-        );
-        $this->registerTagAttribute(
-            'placeholder',
-            'string',
-            'The placeholder of the textfield'
-        );
-        $this->registerTagAttribute(
-            'pattern',
-            'string',
-            'HTML5 validation pattern'
-        );
-        $this->registerArgument(
-            'errorClass',
-            'string',
-            'CSS class to set if there are errors for this ViewHelper',
-            false,
-            'f3-form-error'
-        );
-        $this->registerUniversalTagAttributes();
-        $this->registerArgument(
-            'required',
-            'bool',
-            'If the field is required or not',
-            false,
-            false
-        );
-        $this->registerArgument(
-            'type',
-            'string',
-            'The field type, e.g. "text", "email", "url" etc.',
-            false,
-            'text'
-        );
+        $this->registerArgument('errorClass', 'string', 'CSS class to set if there are errors for this ViewHelper', false, 'f3-form-error');
+        $this->registerArgument('required', 'bool', 'If the field is required or not', false, false);
+        $this->registerArgument('type', 'string', 'The field type, e.g. "text", "email", "url" etc.', false, 'text');
     }
 
-    /**
-     * Renders the textfield.
-     *
-     * @return string
-     */
-    public function render()
+    public function render(): string
     {
-        $required = $this->arguments['required'] ?? false;
-        $type = $this->arguments['type'] ?? null;
+        $required = $this->arguments['required'];
+        $type = $this->arguments['type'];
 
         $name = $this->getName();
         $this->registerFieldNameForFormTokenGeneration($name);
