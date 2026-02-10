f7cf7ce52a787c49dab4073a990044b5f9aea8ba
Sebastian Stein <sebastian.stein@in2code.de>
2026-02-10 10:08:22 +0100

    [REFACTOR] refactor UserFieldsOptions
    Relates: https://projekte.in2code.de/issues/77936

--- Classes/UserFunc/UserFieldsOptions.php
+++ Classes/UserFunc/UserFieldsOptions.php
@@ -4,80 +4,76 @@ declare(strict_types=1);
 
 namespace In2code\Femanager\UserFunc;
 
-use In2code\Femanager\Utility\StringUtility;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\Localization\LanguageService;
+use TYPO3\CMS\Core\Localization\LanguageServiceFactory;
+use TYPO3\CMS\Core\Schema\Struct\SelectItem;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 
-/**
- * Class UserFieldsOptions
- */
 class UserFieldsOptions
 {
-    /**
-     * @var LanguageService
-     */
-    protected $languageService;
+    protected LanguageService $languageService;
+    protected string $localLangPrefix = 'LLL:EXT:femanager/Resources/Private/Language/locallang_db.xlf:';
 
-    /**
-     * @var string
-     */
-    protected $localLangPrefix = 'LLL:EXT:femanager/Resources/Private/Language/locallang_db.xlf:';
+    public function __construct(protected readonly LanguageServiceFactory $languageServiceFactory)
+    {
+        $this->languageService = $this->languageServiceFactory->create('default');
+    }
 
     /**
      * Add options to FlexForm Selection - Options can be defined in TSConfig
      */
     public function addOptions(array &$params): void
     {
-        $tSconfig = BackendUtility::getPagesTSconfig($params['effectivePid'] ?? 0);
         $this->addCaptchaOption($params);
         $this->addStateOption($params);
-        $tab = $params['config']['itemsProcFuncTab'] . '.';
-        if (!empty($tSconfig['tx_femanager.']['flexForm.'][$tab]['addFieldOptions.'])) {
-            $options = $tSconfig['tx_femanager.']['flexForm.'][$tab]['addFieldOptions.'];
-            foreach ((array)$options as $value => $label) {
-                $params['items'][] = [
-                    StringUtility::startsWith($label, 'LLL:') ? $this->languageService->sL($label) : $label,
-                    $value,
-                ];
+
+        $pid = (int)($params['effectivePid'] ?? 0);
+        $tSconfig = BackendUtility::getPagesTSconfig($pid);
+
+        $tab = $params['config']['itemsProcFuncTab'] ?? '';
+        $fieldOptions = $tSconfig['tx_femanager.']['flexForm.'][$tab . '.']['addFieldOptions.'] ?? [];
+
+        if (empty($fieldOptions)) {
+            return;
+        }
+
+        foreach ($fieldOptions as $value => $label) {
+            if (str_ends_with((string)$value, '.')) {
+                continue;
             }
+
+            $translatedLabel = str_starts_with((string)$label, 'LLL:')
+                ? $this->languageService->sL($label)
+                : $label;
+
+            $params['items'][] = new SelectItem(
+                'select',
+                $translatedLabel,
+                $value,
+            );
         }
     }
 
-    /**
-     * Add captcha option
-     */
-    protected function addCaptchaOption(array &$params)
+    protected function addCaptchaOption(array &$params): void
     {
         if (ExtensionManagementUtility::isLoaded('sr_freecap')) {
-            $params['items'][] = [
+            $params['items'][] = new SelectItem(
+                'select',
                 $this->languageService->sL($this->localLangPrefix . 'tx_femanager_domain_model_user.captcha'),
-                'captcha',
-            ];
+                'captcha'
+            );
         }
     }
 
-    /**
-     * Add captcha option
-     */
-    protected function addStateOption(array &$params)
+    protected function addStateOption(array &$params): void
     {
         if (ExtensionManagementUtility::isLoaded('static_info_tables')) {
-            $params['items'][] = [
+            $params['items'][] = new SelectItem(
+                'select',
                 $this->languageService->sL($this->localLangPrefix . 'tx_femanager_domain_model_user.state'),
-                'state',
-            ];
+                'state'
+            );
         }
-    }
-
-    /**
-     * Initialize
-     *
-     * @SuppressWarnings(PHPMD.Superglobals)
-     */
-    protected function initialize()
-    {
-        $this->languageService = $GLOBALS['LANG'];
     }
 }
