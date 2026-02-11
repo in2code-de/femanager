f7e0357f974ae815d3561ea3338464b327f81ca9
Sebastian Stein <sebastian.stein@in2code.de>
2026-02-11 12:22:02 +0100

    [BUGFIX] remove feature check from isAdminAuthentication()
    Relates: https://projekte.in2code.de/issues/77936

--- Classes/Utility/BackendUserUtility.php
+++ Classes/Utility/BackendUserUtility.php
@@ -17,10 +17,7 @@ class BackendUserUtility extends AbstractUtility
     public static function isAdminAuthentication()
     {
         $userAuthentication = self::getBackendUserAuthentication();
-        $isUserBackendEnableLoginAs =
-            $userAuthentication->getTSConfig()['tx_femanager.']['UserBackend.']['enableLoginAs'] ?? 0;
-
-        return $userAuthentication->user['admin'] === 1 || (int)$isUserBackendEnableLoginAs === 1;
+        return $userAuthentication->user['admin'] === 1;
     }
 
     /**
