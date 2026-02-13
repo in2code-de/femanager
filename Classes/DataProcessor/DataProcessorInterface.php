b653cf8656bf24034ff14e07cc5a3c1d18e3c62d
Sebastian Stein <sebastian.stein@in2code.de>
2026-02-13 15:49:29 +0100

    [TASK] add deprecation annotations
    Relates: https://projekte.in2code.de/issues/77936

--- Classes/DataProcessor/DataProcessorInterface.php
+++ Classes/DataProcessor/DataProcessorInterface.php
@@ -6,6 +6,9 @@ namespace In2code\Femanager\DataProcessor;
 
 interface DataProcessorInterface
 {
+    /**
+     * @deprecated function signature will chance in V14
+     */
     public function initializeDataProcessor();
 
     public function process(): void;
--- Classes/Finisher/FinisherInterface.php
+++ Classes/Finisher/FinisherInterface.php
@@ -8,15 +8,33 @@ interface FinisherInterface
 {
     public function getUser();
 
+    /**
+     * @deprecated function signature will chance in V14
+     */
     public function setUser($user);
 
+    /**
+     * @deprecated function name will chance in V14 to getTypoScriptSettings()
+     */
     public function getSettings();
 
+    /**
+     * @deprecated function name and signature will chance in V14 to setTypoScriptSettings(array $typoScriptSettings): FinisherInterface;
+     */
     public function setSettings($settings);
 
+    /**
+     * @deprecated function signature will chance in V14
+     */
     public function getActionMethodName();
 
+    /**
+     * @deprecated function signature will chance in V14
+     */
     public function setActionMethodName(string $actionMethodName);
 
+    /**
+     * @deprecated function signature will chance in V14
+     */
     public function initializeFinisher();
 }
