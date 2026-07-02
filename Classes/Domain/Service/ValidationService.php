in2code-de/femanager

commit 9da55ba21e5e016ac3a97bb3aeb854fe08dfe17c

[BUGFIX] Show correct message on server side validation error
The server side validation always returned the error message of `validationErrorUniqueDb` instead of the actual error message determined by the `ValidationService` if a server side validation error occurs.

This was broken since the introduction of the `ValidationService` in 60e84770 `[BUGFIX] execute serversideValidator user after modifications`

Classes/Domain/Service/ValidationService.php
@@ -34,7 +34,7 @@ class ValidationService
                 foreach ($errors as $error) {
                     $validationErrors[] = GeneralUtility::makeInstance(
                         FlashMessage::class,
-                        LocalizationUtility::translate('validationErrorUniqueDb', 'femanager', $error->getArguments()),
+                        LocalizationUtility::translate($error['message'], 'femanager', $error->getArguments()),
                         $error->getTitle(),
                         ContextualFeedbackSeverity::ERROR,
                     );

commit 9ae08d0d4e1910800e27ef88bd63b39cffe735ed

[BUGFIX] Fix PHP warning when showing message on server side validation error
This was broken since 9da55ba2 `[BUGFIX] Show correct message on server side validation error`

Classes/Domain/Service/ValidationService.php
@@ -34,7 +34,7 @@ class ValidationService
                 foreach ($errors as $error) {
                     $validationErrors[] = GeneralUtility::makeInstance(
                         FlashMessage::class,
-                        LocalizationUtility::translate($error['message'], 'femanager', $error->getArguments()),
+                        LocalizationUtility::translate($error->getMessage(), 'femanager', $error->getArguments()),
                         $error->getTitle(),
                         ContextualFeedbackSeverity::ERROR,
                     );