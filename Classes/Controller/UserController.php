a25a0a0ca751ea996c1e69bf3a0df8ee4457f330
Christine Zoglmeier <christine.zoglmeier@in2code.de>
2026-08-20 12:24:26 +0200

    [SECURITY] Prevent user disclosure via user argument in showAction
    getUser() now resolves the target from settings.show.user and the request argument is ignored.
    When nothing is configured, the requested user is only accepted with a valid HMAC that listAction generates per rendered user
    
    Resolves: https://projekte.in2code.de/issues/82571
    Fixes: https://github.com/in2code-de/femanager/security/advisories/GHSA-pg62-792x-wgfq

--- Classes/Controller/UserController.php
+++ Classes/Controller/UserController.php
@@ -10,6 +10,7 @@ use In2code\Femanager\Domain\Validator\ClientsideValidator;
 use In2code\Femanager\Event\ImpersonateEvent;
 use In2code\Femanager\Utility\BackendUserUtility;
 use In2code\Femanager\Utility\FrontendUtility;
+use In2code\Femanager\Utility\HashUtility;
 use In2code\Femanager\Utility\LocalizationUtility;
 use In2code\Femanager\Utility\UserUtility;
 use Psr\Http\Message\ResponseInterface;
@@ -27,13 +28,20 @@ class UserController extends AbstractFrontendController
 {
     public function listAction(array $filter = []): ResponseInterface
     {
+        $users = $this->userRepository->findByUsergroups(
+            $this->settings['list']['usergroup'] ?? '',
+            $this->settings,
+            $filter
+        );
+        $showHashes = [];
+        foreach ($users as $user) {
+            $showHashes[$user->getUid()] = HashUtility::createHashForUser($user, 'show');
+        }
+
         $this->view->assignMultiple(
             [
-                'users' => $this->userRepository->findByUsergroups(
-                    $this->settings['list']['usergroup'] ?? '',
-                    $this->settings,
-                    $filter
-                ),
+                'users' => $users,
+                'showHashes' => $showHashes,
                 'filter' => $filter,
             ]
         );
@@ -41,9 +49,13 @@ class UserController extends AbstractFrontendController
         return $this->htmlResponse();
     }
 
-    public function showAction(User $user = null): ResponseInterface
+    public function showAction(User $user = null, string $hash = ''): ResponseInterface
     {
-        $this->view->assign('user', $this->getUser($user));
+        $user = $this->getUser($user, $hash);
+        $this->view->assignMultiple([
+            'user' => $user,
+            'showHash' => $user instanceof User ? HashUtility::createHashForUser($user, 'show') : '',
+        ]);
         $this->assignForAll();
         return $this->htmlResponse();
     }
@@ -150,17 +162,17 @@ class UserController extends AbstractFrontendController
         return new RedirectResponse($redirectUri);
     }
 
-    /**
-     * @return User
-     */
-    protected function getUser(User $user = null)
+    protected function getUser(User $user = null, string $hash = '')
     {
-        if ($user === null) {
-            if (is_numeric($this->settings['show']['user'])) {
-                $user = $this->userRepository->findByUid($this->settings['show']['user']);
-            } elseif ($this->settings['show']['user'] === '[this]') {
-                $user = $this->user;
-            }
+        $configuredUser = $this->settings['show']['user'] ?? '';
+        if (is_numeric($configuredUser)) {
+            return $this->userRepository->findByUid($configuredUser);
+        }
+        if ($configuredUser === '[this]') {
+            return $this->user;
+        }
+        if ($user instanceof User && !HashUtility::validHash($hash, $user, 'show')) {
+            throw new UnauthorizedException('Unauthorized user detail request', 1754916601);
         }
         return $user;
     }
